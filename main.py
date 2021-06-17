3a614b21d7e9e157646270934642b2d5df5be3ad
Julio Pinto Coelho <juliopcrj@gmail.com>
2021-06-17 12:00:01 -0300

    adding queries, and stuff

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import sys
 import mysql.connector as sql
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials as SAC
+from config import *
+from queries import *
 
+class Autometrics:
+
+    def __init__(self):
+        try:
+            self.database = sql.connect(
+                    host = host,
+                    user = user,
+                    password = password,
+                    database = dbname
+                    )
+        except:
+            sys.exit("Couldn't connect to the database, please review your config file")
+
+    def __del__(self):
+        print("Closing connection with database")
+        self.database.close()
+
+
+    def test(self):
+        print(self.database)
+
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    Autometrics().test()
 
---
+++ queries.py
@@ -0,0 +1,90 @@
+participants = '''
+SELECT
+  count(id)
+  FROM
+    Participant
+    WHERE
+      currentParticipantState = "ENROLLED"
+        AND site_id = <SITE_ID>;
+'''
+installed = '''
+SELECT
+  COUNT(DISTINCT a.participant_id)
+  FROM
+    VigilanteActionsLog a
+      JOIN Participant p ON p.id = a.participant_id
+      WHERE
+        p.currentParticipantState LIKE '%ENR%'
+        AND (DATE(a.receptionDate) < date(current_date())
+        AND p.site_id = <SITE_ID>;
+        
+'''
+churn = '''
+SELECT
+  responderam.Churn,
+  count(distinct a.participant_id)
+from
+  VigilanteActionsLog a
+  JOIN Participant p ON p.id = a.participant_id
+  JOIN Site s ON s.id = p.site_id
+  LEFT join (
+    SELECT
+      a.participant_id,
+      "No" AS Churn
+    from
+      VigilanteActionsLog a
+      JOIN Participant p ON p.id = a.participant_id
+      JOIN Site s ON s.id = p.site_id
+    WHERE
+      p.currentParticipantState LIKE '%ENR%'
+      AND (
+        DATE(a.receptionDate) BETWEEN DATE(current_date() - interval 7 day)
+        AND DATE(current_date())
+      )
+      AND p.site_id = <SITE_ID>
+    GROUP BY
+      a.participant_id
+  ) AS responderam ON responderam.participant_id = a.participant_id
+WHERE
+  p.currentParticipantState LIKE '%ENR%'
+  AND (
+    DATE(a.receptionDate) BETWEEN DATE(current_date() - interval 14 day)
+    AND DATE(current_date() - interval 8 day)
+  )
+  AND p.site_id = <SITE_ID>
+GROUP BY
+  responderam.Churn
+
+'''
+positive = '''
+SELECT
+  V_Instance.answer,
+    COUNT(DISTINCT V_Instance.participant_id)
+    FROM
+      VigilanteActionsLog AS V_Instance
+        JOIN Participant p ON p.id = V_Instance.participant_id
+          JOIN Site s ON s.id = p.site_id
+          WHERE
+            p.currentParticipantState LIKE '%ENR%'
+              AND (
+                  DATE(V_Instance.receptionDate) BETWEEN DATE(current_date() - interval 7 day)
+                  AND DATE(current_date())
+                )
+              AND V_Instance.answer = "yes"
+              AND p.site_id = <SITE_ID>
+'''
+reports = '''
+SELECT
+  COUNT(DISTINCT a.participant_id)
+  from
+    VigilanteActionsLog a
+      JOIN Participant p ON p.id = a.participant_id
+        JOIN Site s ON s.id = p.site_id
+        WHERE
+          p.currentParticipantState LIKE '%ENR%'
+            AND (
+                DATE(a.receptionDate) BETWEEN DATE(current_date() - interval 7 day)
+                    AND DATE(current_date())
+                  )
+            AND p.site_id = <SITE_ID>
+'''
