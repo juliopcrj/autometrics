5c7ebc3848f440ef7d7fa919d0aec054578dfec4
Julio Pinto Coelho <juliopcrj@gmail.com>
2021-08-20 10:02:38 -0300

    adicionando envio de notificações

--- main.py
+++ main.py
@@ -4,38 +4,43 @@ import gspread
 from gspread.utils import a1_to_rowcol, rowcol_to_a1
 from config import *
 import queries
+from pushbullet import Pushbullet
+from pb import key
 
 sites_sheet = [ "CRIE-UNIFESP", "CPCLIN", "RIO - IDOR", "UFSM", "BAHIA - IDOR" ]
-#general_sheet = "https://docs.google.com/spreadsheets/d/1TvXq18udAPxT52-SbcwXWLqYTjgXiqTtUp6tp5-vJIo/edit#gid=0"
-general_sheet = "https://docs.google.com/spreadsheets/d/1pSPtmqsWKV6PUejD_BlrX3ND1umhbCyJuYcWbI9-Zvs/edit#gid=0"
+general_sheet = "https://docs.google.com/spreadsheets/d/1TvXq18udAPxT52-SbcwXWLqYTjgXiqTtUp6tp5-vJIo/edit#gid=0"
 sites_ids = ['1', '2', '3', '5', '4']
 begin_date = "2021-06-27"
 date_query = 'select current_date() - interval 7 day, current_date() - interval 1 day'
 
+participants_line = 5
+installs_line = 6
+churn_line = 11
+positive_line = 13
+report_line = 14
 
-_participants_line = 5
-_installed_line = 6
-_churn_line = 11
-_positive_line = 13
-_reports_line = 14
-
-_adherence_line = 9
-_installed_rate_line = 10
-_new_users_line = 12
-_effort_saved_line = 15
+adherence_line = 9
+newusers_line = 12
+effort_line = 15
+valid_line = 8
+rate_installs_line = 10
 
 
 class Autometrics:
 
     def __init__(self):
+        self.pb = Pushbullet(key)
+
         try:
             self.database = sql.connect(
                     host = host,
-                    user = user,
+                    user = user, 
                     password = password,
                     database = dbname
                     )
         except:
+            _ = self.pb.push_note("Alert", "Nao executou autometrics")
+
             sys.exit("Couldn't connect to the database, please review your config file")
 
         # This block is just to retrieve the column in which the data should be written
@@ -85,33 +90,34 @@ class Autometrics:
             # begins fetching and writing the new data
             self.cursor.execute(_participants)
             res = self.cursor.fetchone()
-            worksheet.update_cell(_participants_line, self.editing_column, res[0])
+            worksheet.update_cell(participants_line, self.editing_column, res[0])
 
             self.cursor.execute(_installed)
             res = self.cursor.fetchone()
-            worksheet.update_cell(_installed_line, self.editing_column, res[0])
+            worksheet.update_cell(installs_line, self.editing_column, res[0])
 
             self.cursor.execute(_churn)
             res = self.cursor.fetchone()
-            worksheet.update_cell(_churn_line, self.editing_column, res[0])
+            worksheet.update_cell(churn_line, self.editing_column, res[0])
 
             self.cursor.execute(_positive)
             res = self.cursor.fetchone()
-            worksheet.update_cell(_positive_line, self.editing_column, res[0])
+            worksheet.update_cell(positive_line, self.editing_column, res[0])
 
             self.cursor.execute(_reports)
             res = self.cursor.fetchone()
-            worksheet.update_cell(_reports_line, self.editing_column, res[0])
+            worksheet.update_cell(report_line, self.editing_column, res[0])
             # end of fetch and write new data
 
             # writes the formulas
-            worksheet.update(rowcol_to_a1(_adherence_line, self.editing_column), "="+rowcol_to_a1(_reports_line, self.editing_column)+"/"+rowcol_to_a1(_installed_line, self.editing_column), raw=False)
-            worksheet.update(rowcol_to_a1(_installed_rate_line, self.editing_column), "="+rowcol_to_a1(_installed_line, self.editing_column) +"/"+rowcol_to_a1(_participants_line,self.editing_column), raw=False)
-            worksheet.update(rowcol_to_a1(_new_users_line,self.editing_column), "="+rowcol_to_a1(_installed_line,self.editing_column)+"-"+rowcol_to_a1(_installed_line,self.editing_column-1), raw=False)
-            worksheet.update(rowcol_to_a1(_effort_saved_line,self.editing_column),
-                    "=("+rowcol_to_a1(_reports_line, self.editing_column) + "-" +rowcol_to_a1(_positive_line, self.editing_column) +")/"+rowcol_to_a1(_reports_line,self.editing_column),raw=False)
+            worksheet.update(rowcol_to_a1(adherence_line, self.editing_column), "="+rowcol_to_a1(report_line, self.editing_column)+"/"+rowcol_to_a1(participants_line, self.editing_column), raw=False)
+            worksheet.update(rowcol_to_a1(rate_installs_line, self.editing_column), "="+rowcol_to_a1(installs_line, self.editing_column) +"/"+rowcol_to_a1(participants_line,self.editing_column), raw=False)
+            worksheet.update(rowcol_to_a1(newusers_line,self.editing_column), "="+rowcol_to_a1(installs_line,self.editing_column)+"-"+rowcol_to_a1(installs_line,self.editing_column-1), raw=False)
+            worksheet.update(rowcol_to_a1(effort_line,self.editing_column),
+                    "=("+rowcol_to_a1(report_line, self.editing_column) + "-" +rowcol_to_a1(positive_line, self.editing_column) +")/"+rowcol_to_a1(report_line,self.editing_column),raw=False)
 
 
+        _ = self.pb.push_note("Report", "Atualizou as métricas no COV003")
         self.close()
 
 if __name__ == "__main__":
